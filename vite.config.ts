d8698c9386fd6ab3b653addbf5fde05085d7b5a7
ketan.kadam <ketan.kadam@logixal.com>
2025-09-28 01:49:16 +0530

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? (process.env.GITHUB_REPOSITORY ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/` : '/') : '/',
+  base: mode === 'production' ? '/naivedya-menu-explorer/' : '/',
   server: {
     host: "::",
     port: 8080,
